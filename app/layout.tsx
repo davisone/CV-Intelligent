eb3f25e8cf1f240d7f8c5b072d21aa7562e7e5e1
Evan Davison <evan.davison@supdevinci-edu.fr>
2026-02-11 13:42:35 +0100

    changement de la favicone pour la page d'acceuil

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,11 +17,13 @@ export const metadata: Metadata = {
   manifest: '/manifest.json',
   icons: {
     icon: [
-      { url: '/favicon-48.png', sizes: '48x48', type: 'image/png' },
+      { url: '/favicon.ico', sizes: '32x32', type: 'image/x-icon' },
+      { url: '/favicon-64.png', sizes: '64x64', type: 'image/png' },
       { url: '/icon-192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/icon-512.png', sizes: '512x512', type: 'image/png' },
     ],
     apple: [
-      { url: '/apple-icon.png', sizes: '180x180', type: 'image/png' },
+      { url: '/icon-192.png', sizes: '180x180', type: 'image/png' },
     ],
   },
   alternates: {
